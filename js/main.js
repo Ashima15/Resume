e47d0072b17a4bf2b73031d8b7ccb9d422d6f365
Ashima15 <waliaashima96@gmail.com>
2018-05-08 20:14:27 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready( function() {
+
+	// Logo
+	var $logo 	= $('#logo');
+	 if (location.href.indexOf("#") != -1) {
+        if(location.href.substr(location.href.indexOf("#"))!='#about'){
+        	$logo.show();
+        }
+    }
+    
+	// Show logo 
+	$('#tab-container .tab a').click(function() {
+	  $logo.slideDown('slow');
+	});
+	// Hide logo
+	$('#tab-about').click(function() {
+	  $logo.slideUp('slow');
+	});	
+
+      $('#tab-container').easytabs({
+        animate			: true,
+        updateHash		: true,
+        transitionIn	: 'slideDown',
+        transitionOut	: 'slideUp',
+        animationSpeed	: 600,
+        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
+        });
+    });
